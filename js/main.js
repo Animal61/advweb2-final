7c1bc5d267c601f372bb38be6f13dbd97f01dc42
Kelley Smith <s.kelley.smith@hotmail.com>
2018-04-22 10:27:08 -0400

    Add Wow Slider jQuery components to index.html

---
+++ js/main.js
@@ -0,0 +1,3 @@
+// Call WOWSlider
+
+jQuery('#wowslider-container').wowSlider({effect: 'rotate', prev: '', next: '', duration: 2 * 100, delay: 20 * 100, width: 580, height: 212, autoPlay: true, stopOnHover: false, loop: false, bullets: true, caption: true, captionEffect: 'slide', controls: true, logo: '', images: 0});
