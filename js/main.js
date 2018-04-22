c1e8f2dd2fa3d86b558b6639d63d859d8441c13f
Kelley Smith <s.kelley.smith@hotmail.com>
2018-04-22 16:47:39 -0400

    Add services.html stylings and minor mods to js and html files

--- js/main.js
+++ js/main.js
@@ -1,6 +1,9 @@
-// Call WOWSlider
+// Call WOWSlider jQuery plugin
 
 $('#wowslider-container').wowSlider({effect: 'rotate', prev: '', next: '', duration: 2 * 100, delay: 20 * 100, width: 580, height: 212, autoPlay: true, stopOnHover: false, loop: false, bullets: true, caption: true, captionEffect: 'slide', controls: true, logo: '', images: 0});
 
-// Call Parsley validator
+// Call Parsley validator jQuery plugin
 $('#request').parsley();
+
+// Call Tabslet JQuery plugin
+$('.tabs').tabslet();
